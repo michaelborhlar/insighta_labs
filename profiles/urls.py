757a6b753a3249b3a24f4f85b8a782ed4e5d04ed
michaelborhlar <michealadefehinti09@gmail.com>
2026-04-24 10:18:41 +0100

    Fix URL patterns for profile views

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProfileListView, ProfileSearchView
 
 urlpatterns = [
-    path('profiles/search', ProfileSearchView.as_view(), name='profile-search'),
-    path('profiles', ProfileListView.as_view(), name='profile-list'),
+    path('profiles/search/', ProfileSearchView.as_view(), name='profile-search'),
+    path('profiles/', ProfileListView.as_view(), name='profile-list'),
 ]
